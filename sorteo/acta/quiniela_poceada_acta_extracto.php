eb81c2933f456fda78cf98bce6cb22bbd8bd729e
Emma <emmanuel.quattropani@loteriacba.com.ar>
2019-10-16 06:56:08 -0300

    ##64182## Nuevo Juego Quiniela Poceada Correcciones Solicitadas por Marcelo Peñaflor - Cambios en el acta final, pantalla del operador y datos importados.

--- sorteo/acta/quiniela_poceada_acta_extracto.php
+++ sorteo/acta/quiniela_poceada_acta_extracto.php
@@ -14,38 +14,38 @@ $pdf    = new PDF('P');
 $pdf->AliasNbPages();
 $pdf->AddPage();
 try {
-    $rs_sorteo = sql("	SELECT 	TE.DESCRIPCION AS ESCRIBANO,
-							  	TOP.DESCRIPCION     AS OPERADOR,
-							  	TJ.DESCRIPCION      AS JEFE,
-							  	TJT.DESCRIPCION AS TIPO_JUEGO,
-							  	TO_CHAR(TS.FECHA_SORTEO,'dd/mm/YYYY') as FECHA_SORTEO,
-							  	TO_CHAR(TS.FECHA_SORTEO,'HH24:MI:SS') AS HORA_SORTEO,
-							  	TO_CHAR(TS.FECHA_HASTA_PAGO_PREMIO,'DD/MM/YYYY') AS FECHA_CADUCIDAD,
-							  	TS.SORTEO
-						FROM 	SGS.T_SORTEO TS,
-							  	SGS.T_ESCRIBANO TE,
-							  	SUPERUSUARIO.USUARIOS TOP,
-							  	SUPERUSUARIO.USUARIOS TJ,
-							  	SGS.T_JUEGO_TIPO TJT
-						WHERE TS.ID_ESCRIBANO 	  = TE.ID_ESCRIBANO
-							AND TS.ID_OPERADOR    = TOP.ID_USUARIO
-							AND TS.ID_JEFE        = TJ.ID_USUARIO
-							AND TS.ID_TIPO_JUEGO  = TJT.ID_JUEGO_TIPO
-							AND TS.SORTEO         = ?
-							AND TS.ID_JUEGO 	  = ?", array($_SESSION['sorteo'], $_SESSION['id_juego']));
+    $rs_sorteo = sql("  SELECT  TE.DESCRIPCION AS ESCRIBANO,
+                                TOP.DESCRIPCION     AS OPERADOR,
+                                TJ.DESCRIPCION      AS JEFE,
+                                TJT.DESCRIPCION AS TIPO_JUEGO,
+                                TO_CHAR(TS.FECHA_SORTEO,'dd/mm/YYYY') as FECHA_SORTEO,
+                                TO_CHAR(TS.FECHA_SORTEO,'HH24:MI:SS') AS HORA_SORTEO,
+                                TO_CHAR(TS.FECHA_HASTA_PAGO_PREMIO,'DD/MM/YYYY') AS FECHA_CADUCIDAD,
+                                TS.SORTEO
+                        FROM    SGS.T_SORTEO TS,
+                                SGS.T_ESCRIBANO TE,
+                                SUPERUSUARIO.USUARIOS TOP,
+                                SUPERUSUARIO.USUARIOS TJ,
+                                SGS.T_JUEGO_TIPO TJT
+                        WHERE TS.ID_ESCRIBANO     = TE.ID_ESCRIBANO
+                            AND TS.ID_OPERADOR    = TOP.ID_USUARIO
+                            AND TS.ID_JEFE        = TJ.ID_USUARIO
+                            AND TS.ID_TIPO_JUEGO  = TJT.ID_JUEGO_TIPO
+                            AND TS.SORTEO         = ?
+                            AND TS.ID_JUEGO       = ?", array($_SESSION['sorteo'], $_SESSION['id_juego']));
 } catch (exception $e) {
     die($db->ErrorMsg());
 }
 
 try {
-    $rs_extracciones = sql("	SELECT TD.DESCRIPCION,LPAD(TE.NUMERO,2,0) AS NUMERO,TE.POSICION
-						FROM 	SGS.T_EXTRACCION TE,
-								SGS.T_PREMIO_DESCRIPCION TD
-						WHERE TE.POSICION =TD.ID_PREMIO_DESC
-							AND TE.SORTEO = ?
-							AND TE.ID_JUEGO = ?
+    $rs_extracciones = sql("    SELECT TD.DESCRIPCION,LPAD(TE.NUMERO,2,0) AS NUMERO,TE.POSICION
+                        FROM    SGS.T_EXTRACCION TE,
+                                SGS.T_PREMIO_DESCRIPCION TD
+                        WHERE TE.POSICION =TD.ID_PREMIO_DESC
+                            AND TE.SORTEO = ?
+                            AND TE.ID_JUEGO = ?
                             AND (SORTEO_ASOC LIKE ('%QUINIELA ASOCIADA%') OR SORTEO_ASOC LIKE ('%VALIDA%'))
-						ORDER BY TE.POSICION", array($_SESSION['sorteo'], $_SESSION['id_juego']));
+                        ORDER BY TE.POSICION", array($_SESSION['sorteo'], $_SESSION['id_juego']));
 } catch (exception $e) {
     die($db->ErrorMsg());
 }
@@ -57,28 +57,28 @@ $xx         = 110;
 $row_sorteo = $rs_sorteo->FetchNextObject($toupper = true);
 
 $pdf->SetFont('Times', '', 11);
-$pdf->SetXY(120, 50);
+$pdf->SetXY(120, 40);
 $pdf->Cell(30, 5, 'Sorteo Nro:', 0, 0, 'R');
 
-$pdf->SetXY(150, 50);
+$pdf->SetXY(150, 40);
 $pdf->Cell(30, 5, $_SESSION['sorteo'], 1, 0, 'C');
 
-$pdf->SetXY(120, 55);
+$pdf->SetXY(120, 45);
 $pdf->Cell(30, 5, 'Fecha:', 0, 0, 'R');
 
-$pdf->SetXY(150, 55);
+$pdf->SetXY(150, 45);
 $pdf->Cell(30, 5, $row_sorteo->FECHA_SORTEO, 1, 0, 'C');
 
-$pdf->SetXY(120, 60);
+$pdf->SetXY(120, 50);
 $pdf->Cell(30, 5, 'Hora:', 0, 0, 'R');
 
-$pdf->SetXY(150, 60);
+$pdf->SetXY(150, 50);
 $pdf->Cell(30, 5, '    :     ', 1, 0, 'C');
 
-$pdf->SetXY(120, 65);
+$pdf->SetXY(120, 55);
 $pdf->Cell(30, 5, 'Caducidad:', 0, 0, 'R');
 
-$pdf->SetXY(150, 65);
+$pdf->SetXY(150, 55);
 $pdf->Cell(30, 5, $row_sorteo->FECHA_CADUCIDAD, 1, 0, 'C');
 
 $fechasorteo = $row_sorteo->FECHA_SORTEO;
@@ -87,10 +87,10 @@ $texto1      = "En la Ciudad de Córdoba, República Argentina, a los " . substr
 $texto2      = "Con lo que se da por terminado el acto, previa lectura y ratificación de los actuantes, firman la presente por ante mí " . $escribano . " doy fe Escribano Autorizante, de todo lo que certifico; siendo las ............... hs., se da por finalizado el Sorteo.";
 
 $pdf->SetFont('Times', '', 11);
-$pdf->SetXY(25, 80);
+$pdf->SetXY(25, 62);
 $pdf->MultiCell(160, 5, utf8_decode($texto1), 0, 'J', 0, 0);
 
-$pdf->SetXY(25, 145);
+$pdf->SetXY(25, 127);
 try {
     $rs_extracciones_comp = sql("SELECT * FROM(
                                         SELECT
@@ -126,7 +126,8 @@ $ln           = 5;
 $primer_corte = true;
 $pdf->SetX(27);
 $pdf->Cell(6, 5, 'PD', 0, 0, 'C');
-$pdf->Cell(6, 5, utf8_decode('ND'), 0, 0, 'C');
+$pdf->Cell(6, 5, 'ND', 0, 0, 'C');
+$pdf->Cell(6, 5, 'PV', 0, 0, 'C');
 $pdf->Cell(6, 5, '', 0, 0, 'C');
 $pdf->Cell(130, 5, utf8_decode('Extracciones complementarias hasta la extracción valida'), 0, 1, 'L');
 $pdf->SetX(27);
@@ -151,18 +152,23 @@ while ($row_extracciones_comp = $rs_extracciones_comp->FetchNextObject($toupper 
     if (strpos($row_extracciones_comp->SORTEO_ASOC, 'QUINIELA DUPLICADO') !== false && $primer_corte == false) {
         $pdf->ln(5);
         $pdf->SetX(27);
-        $pdf->Cell(6, 5, $row_extracciones_comp->POSICION_DUPLICADO, 1, 0, 'C');
-        $pdf->Cell(6, 5, $row_extracciones_comp->NUMERO, 1, 0, 'C', 1);
+        $pdf->Cell(6, 5, $row_extracciones_comp->POSICION, 1, 0, 'C');
+        $pdf->Cell(6, 5, str_pad($row_extracciones_comp->NUMERO, 2, "0", STR_PAD_LEFT), 1, 0, 'C', 1);
+        $pdf->Cell(6, 5, $row_extracciones_comp->POSICION_DUPLICADO, 1, 0, 'C', 1);
         $pdf->Cell(6, 5, ':', 0, 0, 'C');
     } else {
         if ($primer_corte == true) {
-            $pdf->Cell(6, 5, $row_extracciones_comp->POSICION_DUPLICADO, 1, 0, 'C');
+            $pdf->Cell(6, 5, $row_extracciones_comp->POSICION, 1, 0, 'C');
         }
 
-        $pdf->Cell(6, 5, $row_extracciones_comp->NUMERO, 1, 0, 'C', 1);
+        $pdf->Cell(6, 5, str_pad($row_extracciones_comp->NUMERO, 2, "0", STR_PAD_LEFT), 1, 0, 'C', 1);
+        if ($primer_corte == true) {
+            $pdf->Cell(6, 5, $row_extracciones_comp->POSICION_DUPLICADO, 1, 0, 'C', 1);
+        }
         if ($primer_corte == true) {
             $pdf->Cell(6, 5, ':', 0, 0, 'C');
         }
+
     }
 
     $primer_corte = false;
@@ -177,8 +183,8 @@ $pdf->Line($strikeout_x, $strikeout_y, $strikeout_x + 3, $strikeout_y);
 $pdf->ln(7);
 $pdf->SetX(25);
 $pdf->SetTextColor(0, 0, 0);
-$pdf->SetFont('Times', '', 11);
-$pdf->Cell(100, 5, utf8_decode('Referencia PD: Posición Duplicada, ND: Número Duplicado'), 0, 1, 'L');
+$pdf->SetFont('Times', 'B', 11);
+$pdf->Cell(100, 5, utf8_decode('Referencia PD: Posición Duplicada, ND: Número Duplicado, PV: Posición Validada'), 0, 1, 'L');
 
 $pdf->SetXY(25, 230);
 $pdf->SetFont('Times', '', 11);
@@ -206,7 +212,7 @@ $pdf->Cell(25, 5, utf8_decode($row_sorteo->JEFE), 0, 0, 'C');
 $pdf->SetXY(162, 270);
 $pdf->Cell(25, 5, utf8_decode($row_sorteo->ESCRIBANO), 0, 0, 'C');
 
-$pdf->SetXY(45, 120);
+$pdf->SetXY(45, 103);
 $jj = 0;
 
 $y_inicio = $pdf->GetY();
@@ -220,8 +226,9 @@ while ($row = $rs_extracciones->FetchNextObject($toupper = true)) {
 
     }
     $pdf->SetX($x);
+    $pdf->SetFont('Times', 'B', 11);
+    $pdf->Cell(20, 5, ucwords(strtolower('POSICION ' . $row->POSICION)), 1, 0, 'C');
     $pdf->SetFont('Times', '', 11);
-    $pdf->Cell(20, 5, ucwords(strtolower('POSICION ' . $row->POSICION)), 1, 0, 1);
     $pdf->Cell(20, 5, $row->NUMERO, 1, 1, 'C');
     $jj += 1;
 }
