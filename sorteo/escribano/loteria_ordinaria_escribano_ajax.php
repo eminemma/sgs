594c811c410ef1efe3121385fae2023f9798afb6
Emma <emmanuel.quattropani@loteriacba.com.ar>
2021-09-13 08:57:37 -0300

    ##80248## : RE: problema en la exportacion del sorteo - Arreglo posterior a la implementacion

--- sorteo/escribano/loteria_ordinaria_escribano_ajax.php
+++ sorteo/escribano/loteria_ordinaria_escribano_ajax.php
@@ -1,8 +1,7 @@
 <?php
 @session_start();
-include_once dirname(__FILE__).'/../../db.php';
-include_once dirname(__FILE__).'/../../librerias/alambre/funcion.inc.php';
-
+include_once dirname(__FILE__) . '/../../db.php';
+include_once dirname(__FILE__) . '/../../librerias/alambre/funcion.inc.php';
 
 $res = sql("SELECT
 				VALOR
@@ -11,21 +10,21 @@ $res = sql("SELECT
 			WHERE
 				ID_JUEGO = ?
 				AND PARAMETRO = 'ZONA_MOSTRANDO'",
-			array($_SESSION['id_juego']));
+    array($_SESSION['id_juego']));
 
 $row = siguiente($res);
 
 $retorno = array(
-					'zonaMostrando' => 'zona'.(int)$row->VALOR,
-					'billetesZona1' => array(),
-					'billetesZona2' => array(),
-					'billetesZona3' => array(),
-					'datosSorteo'   => array()
-				);
+    'zonaMostrando' => 'zona' . (int) $row->VALOR,
+    'billetesZona1' => array(),
+    'billetesZona2' => array(),
+    'billetesZona3' => array(),
+    'datosSorteo'   => array(),
+);
 
 /**
-	BUSCAMOS LAS EXTRACCIONES DE LA ZONA 1
-*/
+BUSCAMOS LAS EXTRACCIONES DE LA ZONA 1
+ */
 
 $res = sql("SELECT
 				LPAD(NUMERO, 5, 0) AS NUMERO,
@@ -41,43 +40,45 @@ $res = sql("SELECT
 				AND ZONA_JUEGO = 1
 			ORDER BY
 				ORDEN",
-			array($_SESSION['id_juego'], $_SESSION['sorteo']));
+    array($_SESSION['id_juego'], $_SESSION['sorteo']));
 
-while($row = siguiente($res)){
-	$retorno['billetesZona1'][] = array('numero' => $row->NUMERO, 'posicion' => $row->POSICION, 'vendido' => $row->VENDIDO);
+while ($row = siguiente($res)) {
+    $retorno['billetesZona1'][] = array('numero' => $row->NUMERO, 'posicion' => $row->POSICION, 'vendido' => $row->VENDIDO);
 }
 
-
-$res = sql("SELECT 	TE.DESCRIPCION AS ESCRIBANO,
-				  	TOP.DESCRIPCION     AS OPERADOR,
-				  	TJ.DESCRIPCION      AS JEFE,
-				  	TJT.DESCRIPCION AS TIPO_JUEGO,
-				  	TO_CHAR(TS.FECHA_SORTEO,'dd/mm/YYYY') as FECHA_SORTEO,
-				  	TO_CHAR(TS.FECHA_SORTEO,'HH24:MI') AS HORA_SORTEO,
-				  	TS.SORTEO
-			FROM 	SGS.T_SORTEO TS,
-				  	SGS.T_ESCRIBANO TE,
-				  	SUPERUSUARIO.USUARIOS TOP,
-				  	SUPERUSUARIO.USUARIOS TJ,
-				  	SGS.T_JUEGO_TIPO TJT
-			WHERE TS.ID_ESCRIBANO 	  = TE.ID_ESCRIBANO
-				AND TS.ID_OPERADOR    = TOP.ID_USUARIO
-				AND TS.ID_JEFE        = TJ.ID_USUARIO
-				AND TS.ID_TIPO_JUEGO  = TJT.ID_JUEGO_TIPO
-				AND TS.SORTEO         = ?
-				AND TS.ID_JUEGO 	  = ?",
-			array($_SESSION['sorteo'],$_SESSION['id_juego']));
+$res = sql("SELECT   TE.DESCRIPCION AS ESCRIBANO,
+                TOP.DESCRIPCION     AS OPERADOR,
+                TJ.DESCRIPCION      AS JEFE,
+                TJT.DESCRIPCION AS TIPO_JUEGO,
+                TO_CHAR(TS.FECHA_SORTEO,'dd/mm/YYYY') as FECHA_SORTEO,
+                TO_CHAR(TS.FECHA_SORTEO,'HH24:MI:SS') AS HORA_SORTEO,
+                TO_CHAR(TS.FECHA_HASTA_PAGO_PREMIO,'DD/MM/YYYY') AS FECHA_CADUCIDAD,
+                TS.SORTEO
+        FROM    SGS.T_SORTEO TS,
+                SGS.T_ESCRIBANO TE,
+                SUPERUSUARIO.USUARIOS TOP,
+                SUPERUSUARIO.USUARIOS TJ,
+                SGS.T_JUEGO_TIPO TJT,
+                SGS.T_PROGRAMA TP
+        WHERE TS.ID_ESCRIBANO     = TE.ID_ESCRIBANO
+            AND TS.ID_OPERADOR    = TOP.ID_USUARIO
+            AND TS.ID_JEFE        = TJ.ID_USUARIO
+            AND TP.ID_PROGRAMA = TS.ID_PROGRAMA
+            AND TP.CODIGO_TIPO_JUEGO = TJT.CODIGO_TIPO_JUEGO
+            AND TS.SORTEO         = ?
+            AND TS.ID_JUEGO       = ?",
+    array($_SESSION['sorteo'], $_SESSION['id_juego']));
 $row = siguiente($res);
 
-$retorno['datosSorteo'] = 	array(    
-	  						    'sorteo'        => ($row->SORTEO==NULL)       ? '' : $row->SORTEO,
-								'fecha_sorteo'  => ($row->FECHA_SORTEO==NULL) ? '' : $row->FECHA_SORTEO,
-								'hora_sorteo'   => ($row->HORA_SORTEO==NULL)  ? '' : $row->HORA_SORTEO,
-								'programa'	   => array()
-							);
+$retorno['datosSorteo'] = array(
+    'sorteo'       => ($row->SORTEO == null) ? '' : $row->SORTEO,
+    'fecha_sorteo' => ($row->FECHA_SORTEO == null) ? '' : $row->FECHA_SORTEO,
+    'hora_sorteo'  => ($row->HORA_SORTEO == null) ? '' : $row->HORA_SORTEO,
+    'programa'     => array(),
+);
 
 $retorno['datosSorteo']['programa'] = array();
-$res = sql("  	SELECT TS.SORTEO,TPD.DESCRIPCION,TPP.PREMIO_EFECTIVO,TPP.ID_DESCRIPCION,TPP.PROGRESION
+$res                                = sql("  	SELECT TS.SORTEO,TPD.DESCRIPCION,TPP.PREMIO_EFECTIVO,TPP.ID_DESCRIPCION,TPP.PROGRESION
   				FROM  	SGS.T_SORTEO TS,
         				SGS.T_PROGRAMA TP,
         				SGS.T_PROGRAMA_PREMIOS TPP,
@@ -87,35 +88,44 @@ $res = sql("  	SELECT TS.SORTEO,TPD.DESCRIPCION,TPP.PREMIO_EFECTIVO,TPP.ID_DESCR
     				AND TP.ID_PROGRAMA = TPP.ID_PROGRAMA
     				AND TPP.ID_DESCRIPCION = TPD.ID_PREMIO_DESC
     			ORDER BY TPP.ID_DESCRIPCION",
-			array($_SESSION['sorteo']));
-while($row = siguiente($res)){
-	if($row->ID_DESCRIPCION == 1){
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "PRIMER PREMIO", "monto" => '$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-		$progresion = '$'.number_format($row->PROGRESION,0,',','.');
-	}	
-	if($row->ID_DESCRIPCION == 2)
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "2º PREMIO", "monto" =>'$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-	if($row->ID_DESCRIPCION == 3)
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "3º PREMIO", "monto" => '$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-	if($row->ID_DESCRIPCION == 4)
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "4º PREMIO", "monto" => '$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-	if($row->ID_DESCRIPCION == 5)
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "5º PREMIO", "monto" =>'$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-	if($row->ID_DESCRIPCION == 6)
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "5 PREMIOS", "monto" => '$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-	if($row->ID_DESCRIPCION == 11){
-		$retorno['datosSorteo']['programa'][] = array("tipo_premio" => "10 PREMIOS", "monto" => '$'.number_format($row->PREMIO_EFECTIVO,0,',','.'));
-		break;
-	}
-		
+    array($_SESSION['sorteo']));
+while ($row = siguiente($res)) {
+    if ($row->ID_DESCRIPCION == 1) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "PRIMER PREMIO", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+        $progresion                           = '$' . number_format($row->PROGRESION, 0, ',', '.');
+    }
+    if ($row->ID_DESCRIPCION == 2) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "2º PREMIO", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+    }
+
+    if ($row->ID_DESCRIPCION == 3) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "3º PREMIO", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+    }
+
+    if ($row->ID_DESCRIPCION == 4) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "4º PREMIO", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+    }
+
+    if ($row->ID_DESCRIPCION == 5) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "5º PREMIO", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+    }
+
+    if ($row->ID_DESCRIPCION == 6) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "5 PREMIOS", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+    }
+
+    if ($row->ID_DESCRIPCION == 11) {
+        $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "10 PREMIOS", "monto" => '$' . number_format($row->PREMIO_EFECTIVO, 0, ',', '.'));
+        break;
+    }
+
 }
 
 $retorno['datosSorteo']['programa'][] = array("tipo_premio" => "PROGRESION", "monto" => $progresion);
 
-
 /**
-	BUSCAMOS LAS EXTRACCIONES DE LA ZONA 2
-*/
+BUSCAMOS LAS EXTRACCIONES DE LA ZONA 2
+ */
 
 $res = sql("SELECT
 				LPAD(NUMERO, 5, 0) AS NUMERO,
@@ -129,17 +139,17 @@ $res = sql("SELECT
 				AND ZONA_JUEGO = 3
 			ORDER BY
 				ORDEN",
-			array($_SESSION['id_juego'], $_SESSION['sorteo']));
+    array($_SESSION['id_juego'], $_SESSION['sorteo']));
 
-while($row = siguiente($res)){
-	$retorno['billetesZona2'][] = array('numero' => $row->NUMERO,
-										'posicion' => $row->POSICION,
-										'fraccion' => $row->FRACCION);
+while ($row = siguiente($res)) {
+    $retorno['billetesZona2'][] = array('numero' => $row->NUMERO,
+        'posicion'                                   => $row->POSICION,
+        'fraccion'                                   => $row->FRACCION);
 }
 
 /**
-	BUSCAMOS EL GANADOR DEL SIEMPRE SALE (ZONA 3)
-*/
+BUSCAMOS EL GANADOR DEL SIEMPRE SALE (ZONA 3)
+ */
 
 $res = sql("SELECT
 
@@ -159,28 +169,27 @@ $res = sql("SELECT
 				AND SORTEO = ?
 				AND ID_PREMIO_DESCRIPCION = 1
         GROUP BY BILLETE,ID_AGENCIA,ID_PREMIO_DESCRIPCION,LOCALIDAD,PROVINCIA,DESCRIPCION_AGENCIA,DESCRIPCION_SUCURSAL,ID_SUCURSAL",
-			array($_SESSION['id_juego'], $_SESSION['sorteo']));
+    array($_SESSION['id_juego'], $_SESSION['sorteo']));
 
-if($res->RecordCount()>0){
-	$localidad = array();
-	while($row = siguiente($res)){
-		$billete=$row->BILLETE;
-		$posicion=$row->POSICION;
-		
-		if($row->DESCRIPCION_AGENCIA=='VENTA MOSTRADOR'){
-			$localidad[]='VENTA MOSTRADOR, '.$row->PROVINCIA;
-		}else if($row->DESCRIPCION_AGENCIA=='VENTA CONTADO'){
-			$localidad[]='VENTA MOSTRADOR, '.$row->PROVINCIA;
-		}else{
-			$localidad[]=utf8_encode(str_pad($row->ID_AGENCIA, 5, "0", STR_PAD_LEFT).' - '.$row->LOCALIDAD.', '.$row->PROVINCIA);
-		}
-	}
+if ($res->RecordCount() > 0) {
+    $localidad = array();
+    while ($row = siguiente($res)) {
+        $billete  = $row->BILLETE;
+        $posicion = $row->POSICION;
 
+        if ($row->DESCRIPCION_AGENCIA == 'VENTA MOSTRADOR') {
+            $localidad[] = 'VENTA MOSTRADOR, ' . $row->PROVINCIA;
+        } else if ($row->DESCRIPCION_AGENCIA == 'VENTA CONTADO') {
+            $localidad[] = 'VENTA MOSTRADOR, ' . $row->PROVINCIA;
+        } else {
+            $localidad[] = utf8_encode(str_pad($row->ID_AGENCIA, 5, "0", STR_PAD_LEFT) . ' - ' . $row->LOCALIDAD . ', ' . $row->PROVINCIA);
+        }
+    }
 
-	$retorno['billetesZona3'][] = array('numero' => $billete,
-										'posicion' => $posicion,
-										'localidad' => $localidad
-										);
+    $retorno['billetesZona3'][] = array('numero' => $billete,
+        'posicion'                                   => $posicion,
+        'localidad'                                  => $localidad,
+    );
 }
 //var_dump($retorno['billetesZona3']);
 echo json_encode($retorno);
@@ -196,7 +205,7 @@ echo '{
 							],
 		"programa"		: [
 							{"tipo_premio" : "PRIMER PREMIO", "monto" : "150000"}
-						  ]					
+						  ]
 
 		"billetesZona2" : 	[
 								{ "numero" : "12345", "posicion" : "03", "fraccion" : "15" },
@@ -211,27 +220,22 @@ echo '{
 	}';
 exit;
 
-
-
-
-
-
 // echo '{
-// 		"zonaMostrando" : "zona1",
+//         "zonaMostrando" : "zona1",
 
-// 		"billetesZona1" : 	[
-// 								{ "numero" : "23455", "posicion" : "01" },
-// 								{ "numero" : "12345", "posicion" : "02" },
-// 								{ "numero" : "02345", "posicion" : "03" }
-// 							],
+//         "billetesZona1" :     [
+//                                 { "numero" : "23455", "posicion" : "01" },
+//                                 { "numero" : "12345", "posicion" : "02" },
+//                                 { "numero" : "02345", "posicion" : "03" }
+//                             ],
 
-// 		"billetesZona2" : 	[
-// 								{ "numero" : "99999", "posicion" : "01", "fraccion" : "11" },
-// 								{ "numero" : "12345", "posicion" : "02", "fraccion" : "07" },
-// 								{ "numero" : "12345", "posicion" : "03", "fraccion" : "15" }
-// 							],
+//         "billetesZona2" :     [
+//                                 { "numero" : "99999", "posicion" : "01", "fraccion" : "11" },
+//                                 { "numero" : "12345", "posicion" : "02", "fraccion" : "07" },
+//                                 { "numero" : "12345", "posicion" : "03", "fraccion" : "15" }
+//                             ],
 
-// 		"billetesZona3" : 	[
-// 								{ "numero" : "12345", "posicion" : "01", "nro_agen" : "00001", "localidad" : "San Antonio de Litin, Córdoba" }
-// 							]
-// 	}';
+//         "billetesZona3" :     [
+//                                 { "numero" : "12345", "posicion" : "01", "nro_agen" : "00001", "localidad" : "San Antonio de Litin, Córdoba" }
+//                             ]
+//     }';
